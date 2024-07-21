9ad8470a0d57545039bf15909f11e25c6c2b609f
Aayush Jha <43124127+avc14github@users.noreply.github.com>
2024-07-21 20:50:37 +0000

    added files in counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // let counter = 0
+
+  const [count , setCount] = useState(0);
+
+  const addValue = () => {
+    if(count < 20){
+      setCount(count + 1)
+    }
+  }
+
+  console.log(count)
+
+  const removeValue = () => {
+    if (count > 0){
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1> Chai aur React </h1>
+      <h2> Counter Value : {count}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
