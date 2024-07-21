78c2905c75f74585c4f4b527bac37689d68c06c3
Aayush Jha <43124127+avc14github@users.noreply.github.com>
2024-07-21 20:53:27 +0000

    modified app.jsx and main.jsx files

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
 import Game from "./Game"
 
 function App() {
+  const add = 29 +30;
+
+  const user = "Aayush"
 
   return (
     <>
-    <h1>React JS with vite | Aayush</h1> 
+    <h1>React JS with vite | {user} | {add}</h1> 
     <Game />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <>
+            <h1>React web page</h1>
+        </>
+    )
+}
+
+const ReactElement = {
+    type : 'a',
+    props: {
+        href : 'https://google.com',
+        target : '_blank'
+    },
+    children : 'Click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = " | Aayush Jha"
+
+const reactElement = React.createElement(
+    'a',
+    {href : 'https://google.com' , target :'_blank'},
+    'Click to visit google',
+    anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
-    
+    reactElement
+
 )
